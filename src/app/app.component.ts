f76a44d4e844bd03769ac6380b502380b2f89f7f
JUAN CARLOS HERNANDEZ HERNANDEZ <juan.carlos.hernandez@ibm.com>
2019-07-18 10:03:32 -0500

    Primera app con angular consumiendo un servicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
 
+import { DataService } from './data.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myfirstapp';
+  //APRENDIENDO TYPESCRIPT
+  //users = ['ryan','joe','cameron','john','bruce'];
+  //activated: boolean = false;
+ /* name: string = 'Ryan Ray';
+  age: number ;
+  address: {
+    street: string;
+    city: string;
+  }
+  hobbies: string[];
+
+  constructor(){
+    this.age = 20;
+    this.address = {
+      street: '221B baker street',
+      city: 'london'
+    }
+    this.hobbies = ['swimming','read','write'];
+  }
+
+  */
+name:string = ' Juan Hernandez';
+age:number = 20;
+users:string[] = ['ryan','joe','cameron'];
+posts = [];
+
+constructor(private dataService: DataService){
+  this.dataService.getData().subscribe(data => {
+    this.posts = data ;
+  }) ;
+}
+
+sayHello(){
+  alert('Hello!');
+}
+
+deleteUser(user){
+  for(let i=0;i < this.users.length; i++){
+    if(user==this.users[i]){
+      this.users.splice(i,1);
+    }
+  }
+}
+
+addUser(newUser){
+  this.users.push(newUser.value);
+  newUser.value = '';
+  newUser.focus();
+  return false;
+}
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+//importar un enrutador
+import { RouterModule, Route } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HelloWorld } from './primercomponente/primer.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { UserComponent } from './user/user.component';
+
+//Asi se importa el servicio y se llama en providers,abajo
+import { DataService } from './data.service';
+import { AboutComponent } from './about/about.component';
+
+
+const routes:Route[] = [
+  {path: '', component: HolaMundoComponent},
+  {path: 'about' , component: AboutComponent}
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorld,
+    HolaMundoComponent,
+    UserComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
